82c2f7167d606e729f113fe9a665c30f7217a1bb
Никита Копылов <nekopylov@edu.hse.ru>
2021-03-05 17:09:28 +0500

    Revert "LastVersion"
    This reverts commit e3528f6b3922ccdfd55ecbd84da035594a1d31e0.

--- Lab3Var8.py
+++ Lab3Var8.py
@@ -47,46 +47,6 @@ if var == 2:
     with open ("Output.txt", "w") as file:
         new_dictionary1 = "".join(new_dictionary)
         file.write(new_dictionary1)
-    print("Задание 5")
-    with open ("nya.txt","r") as nya:
-        nya1 = nya.read()
-        print (nya1)
-        print ("OK")
-    with open ("helloworld.exe","w") as world:
-        print("Access denied")
-if var == 3:
-    #note = {"Буфорд":2000, "Хум":23, "Рабштейн": 25, "Мелник":390, "Калласте": 953, "Дюрбюи": 11000, "Иннополис":96, "Панемуне":265, "Чекалин":965, "Шеффилд":551800,"Лидс":751485,"Кроли":100547, "Сент-Хеленс": 102629, "Олдем": 103544, "Блэкберн":105085}
-    print ("Выберите метод")
-    var1 = input()
-    
-    while not var1.isnumeric():
-        print ("Введите число")
-        var = input("Введите номер метода от 1 до 4: ")
-    var1 = int(var1)
-    if var1 == 1:
-        from pprint import pprint
-        import random
-        spisok = ["Буфорд","Хум","Рабштейн", "Мелник", "Калласте", "Дюрбюи", "Иннополис", "Панемуне","Чекалин",  "Шеффилд", "Лидс", "Кроли", "Сент-Хеленс","Олдем","Блэкберн"]
-        slovar = dict()
-        for t in range (0, len(spisok)):
-            a = random.randint(23, 106000)
-            slovar [t] = {spisok[t]:a}
-        pprint(slovar)
-    elif var1==2:
-        for k in range(0,2):
-            key = input("Введите название города ")
-            population = input("Введите кол-во жителей ")
-            while not population.isnumeric():
-                population = input("Введите целое число: ")
-            population = int(population)
-            if (population<=0):
-                while (population<=0):
-                    print("Мне кажется, ты что-то перепутал, нужно ввести положительное число")
-                    population = input()
-            population = int(population)
-            slovar1 = dict()
-            slovar1 [k] = {key:population}
-            print(k)
-        from pprint import pprint
-        pprint(slovar1)
+
+
     
